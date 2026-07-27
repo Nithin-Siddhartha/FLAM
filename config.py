60fb452240317420d36256c6412efc5b6f236bd8
Nithin_Siddharth <91595243+Nithin-Siddhartha@users.noreply.github.com>
2026-07-27 23:56:09 +0530

    Default case for the retries to be done and backoff base as per the Assignement

---
+++ config.py
@@ -0,0 +1,15 @@
+import json
+import os
+
+CONFIG_PATH = "queuectl.json"
+
+def get_config():
+    if os.path.exists(CONFIG_PATH):
+        with open(CONFIG_PATH, "r") as f:
+            return json.load(f)
+    return {"max_retries": 3, "backoff_base": 2}
+
+
+def save_config(config):
+    with open(CONFIG_PATH, "w") as f:
+        json.dump(config, f, indent=4)
